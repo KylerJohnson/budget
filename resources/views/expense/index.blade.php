552fc57d7a6b11bc4d3826b28f5b854855b2da96
Kyler Johnson <kyler@localhost.localdomain>
2017-05-18 19:24:33 -0700

    Updated master.blade.php and added associated partials and css.

--- resources/views/expense/index.blade.php
+++ resources/views/expense/index.blade.php
@@ -2,60 +2,44 @@
 
 @section('content')
 
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			Overview of current spending (graph)
-			<canvas id="myChart" width="400" height="400"></canvas>
-		</div>
-	</div>
-</div>
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Expenses</div>
-				<div class="panel-body">
-					<p>
-						The list of our expenses is included in the table below.  There should be some more features to talk about soon!
-					</p>
-				</div>
-				<table class="table table-striped">
-					<thead>
-						<tr>
-							<th>Expense Type</th>
-							<th>Amount</th>
-							<th>Description</th>
-							<th>Date</th>
-						</tr>
-					</thead>
-					<tbody>
-						@if (count($expenses)>0)
-							@foreach ($expenses as $expense)
-								<tr>
-									<td>{{ $expense->expense_type->name }}</td>
-									<td>{{ $expense->amount }}</td>
-									<td>{{ $expense->description }}</td>
-									<td>{{ $expense->date }}</td>
-								</tr>
-							@endforeach
-						@else
-							We don't have any expenses to display!  (Should add an "Add some expenses" button)
-						@endif
-					</tbody>
-				</table>
-				<a href="create"><button class="btn btn-primary">Add an expense</button></a>
-				<button>Click </button>
-			</div>
-		</div>
-	</div>
-</div>
-<script>
-/*
-	$('button').on('click', function(){
-		alert('Button click.');
-	});
-	*/
-	</script>
+<div class="row">
+	<div class="col-md-3">
+	Overview of current spending (graph)
 
+	<canvas id="myChart" width="400" height="400"></canvas>
+	<div class="panel panel-default">
+		<div class="panel-heading">Expenses</div>
+		<div class="panel-body">
+			<p>
+				The list of our expenses is included in the table below.  There should be some more features to talk about soon!
+			</p>
+		</div>
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th>Expense Type</th>
+					<th>Amount</th>
+					<th>Description</th>
+					<th>Date</th>
+				</tr>
+			</thead>
+			<tbody>
+				@if (count($expenses)>0)
+					@foreach ($expenses as $expense)
+						<tr>
+							<td>{{ $expense->expense_type->name }}</td>
+							<td>{{ $expense->amount }}</td>
+							<td>{{ $expense->description }}</td>
+							<td>{{ $expense->date }}</td>
+						</tr>
+					@endforeach
+				@else
+					We don't have any expenses to display!  (Should add an "Add some expenses" button)
+				@endif
+			</tbody>
+		</table>
+		<a href="create"><button class="btn btn-primary">Add an expense</button></a>
+		<button>Click </button>
+	</div>
+</div> <!-- div.row -->
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,40 +1,70 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<!-- <link rel="icon" href="../../favicon.ico">-->
 
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>@Budgets - @yield('page_title')</title>
+		<title>Dashboard Template for Bootstrap</title>
 
-		<!-- CSS -->
-		<link rel="stylesheet" href="/css/app.css">
+		<!-- Bootstrap core CSS -->
+		<link href="/css/app.css" rel="stylesheet">
 
-		<style>
-			.header, .footer {
-				text-align: center;
-				padding-top: 30px;
-				padding-bottom: 30px;
-			}
-			.header {
-				font-size: 2em;
-			}
-		</style>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+		<!-- <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet"> -->
 
+		<!-- Custom styles for this template -->
+		<link href="/css/dashboard.css" rel="stylesheet">
 
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<meta name="csrf-token" content="{{ csrf_token() }}">
+
+		<script>
+			window.Laravel = <?php echo json_encode([
+				'csrfToken' => csrf_token(),
+			]); ?>
+		</script>
 	</head>
+
 	<body>
-		<!-- TODO: Put in some sort of nav bar -->
-		<div class="header">
-			Budgets
-		</div>
-		<div id="page_content">
-			@yield('content')
-		<div>
-		<div class="clearfix"></div>
-		<div class="footer">
-			Copyright &copy; Kyler Johnson 2017
-		</div>
-		<!-- JavaScript -->
+
+		@include('layouts.partials.nav')
+
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+
+					@include('layouts.partials.sidebar')
+
+					@yield('content')
+
+				</div><!-- div.col -->
+			</div><!-- div.row -->
+		</div><!-- div.container -->
+
+		<!-- Bootstrap core JavaScript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
 		<script src="/js/app.js"></script>
+		<!-- 
+		<?php /*
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+		<script src="../../dist/js/bootstrap.min.js"></script>
+		<!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+		<script src="../../assets/js/vendor/holder.min.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+		<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+		*/?>
+		-->
 	</body>
 </html>
+
