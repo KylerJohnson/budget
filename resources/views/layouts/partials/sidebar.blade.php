a6cb0f6266c270909d789a9d8a34e3594ad7fe7b
Kyler Johnson <kyler.johnson.01@gmail.com>
2017-06-29 09:32:01 -0700

    Make sidebar links active
    Sidebar links now show active status relative to the page that is
    being viewed.

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,8 +1,18 @@
+<?php
+	$request_parameters = Request::segments();
+?>
+
 <div class="col-sm-3 col-md-2 sidebar">
 	<ul class="nav nav-sidebar">
-		<li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
+		<li {!! $request_parameters[0] == 'expenses' ? 'class="active"':''!!} >
+			<a href="/expenses">
+				Overview
+				@if($request_parameters[0] == 'expenses')
+					<span class="sr-only">(current)</span>
+				@endif
+			</a>
+		</li>
 		<li><a href="#">Reports</a></li>
-		<li><a href="#">Analytics</a></li>
 		<li><a href="#">Export</a></li>
 	</ul>
 	<ul class="nav nav-sidebar">
