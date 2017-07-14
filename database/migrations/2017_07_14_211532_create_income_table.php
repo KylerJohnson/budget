c3577fae32ec6300aee7cf6bf8299eb2ac1be841
Kyler Johnson <kyler.johnson.01@gmail.com>
2017-07-14 14:53:57 -0700

    Updated default incomes to income
    The file used for the migration was incomes.  It has now been changed
    to income.  This includes changing the name of the class inside the
    file.  The default search location for the Income class is incomes.
    This has also been updated to income.

--- database/migrations/2017_07_14_211532_create_income_table.php
+++ database/migrations/2017_07_14_211532_create_income_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateIncomesTable extends Migration
+class CreateIncomeTable extends Migration
 {
     /**
      * Run the migrations.
