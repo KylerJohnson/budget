3e334fc4aa248ba92377a18425ba32a73d7dc708
Kyler Johnson <kyler.johnson.01@gmail.com>
2017-07-28 11:56:11 -0700

    Update expense type edit page
    The expense type update page wasn't redisplaying old form data when
    there were validation errors.  The form now shows all user data
    correctly when the form does not pass validation.

--- resources/views/expense_type/edit.blade.php
+++ resources/views/expense_type/edit.blade.php
@@ -53,62 +53,123 @@
 			{{ method_field('PUT') }}
 			<div class="form-group">
 				<label for="expense_type">Expense Type</label>
-				<input type="text" id="expense_type" name="expense_type" class="form-control" value="{{ $expense_type->name }}">
+				<input type="text" id="expense_type" name="expense_type" class="form-control"
+					@if(old('expense_type'))
+						value="{{ old('expense_type') }}"
+					@else
+						value="{{ $expense_type->name }}"
+					@endif
+				>
+
 			</div>
 			<div class="form-group">
 				<label for="monthly_budget">Monthly Budget</label>
 				<div class="input-group">
 					<span class="input-group-addon">$</span>
-					<input type="text" id="monthly_budget" name="monthly_budget" class="form-control" value="{{ $expense_type->monthly_budget }}">
+					<input type="text" id="monthly_budget" name="monthly_budget" class="form-control"
+						@if(old('monthly_budget'))
+							value="{{ old('monthly_budget') }}"
+						@else
+							value="{{ $expense_type->monthly_budget }}"
+						@endif
+					>
 				</div>
 			</div>
 			<fieldset class="form-group">
 				<legend>Is this a recurring expense?</legend>
 				<div class="radio">
 					<label>
-						<input type="radio" name="recurring_expense" data-toggle_target="recurring_expense_hide" value="1" {{ $expense_type->recurring_expense === "1" ? "checked":"" }}>
+						<input type="radio" name="recurring_expense" data-toggle_target="recurring_expense_hide" value="1"
+							@if(!is_null(old('recurring_expense')))
+								{{ old('recurring_expense') == '1' ? 'checked':'' }}
+							@else
+								{{ $expense_type->recurring_expense == '1' ? 'checked':'' }}
+							@endif
+						>
 						Yes
 					</label>
 				</div>
 				<div class="radio">
 					<label>
-						<input type="radio" name="recurring_expense" data-toggle_target="recurring_expense_hide" value="0" {{ $expense_type->recurring_expense === "0" ? "checked":"" }}>
+						<input type="radio" name="recurring_expense" data-toggle_target="recurring_expense_hide" value="0"
+							@if(!is_null(old('recurring_expense')))
+								{{ old('recurring_expense') === '0' ? 'checked':'' }}
+							@else
+								{{ $expense_type->recurring_expense === '0' ? 'checked':'' }}
+							@endif
+						>
 						No
 					</label>
 				</div>
 			</fieldset>
-			<div class="recurring_expense_hide {{ $expense_type->recurring_expense === "1" ? "" : "display-none" }}">
+			<div class="recurring_expense_hide
+				@if(!is_null(old('recurring_expense')))
+					{{ old('recurring_expense') == '1' ? '':'display-none' }}
+				@else
+					{{ $expense_type->recurring_expense == '1' ? '':'display-none' }}
+				@endif
+			">
 				<div class="form-group">
 					<label for="monthly-amount">Monthly Amount</label>
 					<div class="input-group">
 						<span class="input-group-addon">$</span>
-						<input type="text" id="monthly-amount" name="monthly_amount" class="form-control" value="{{ $expense_type->monthly_amount }}">
+						<input type="text" id="monthly-amount" name="monthly_amount" class="form-control"
+							@if(old('monthly_amount'))
+								value="{{ old('monthly_amount') }}"
+							@else
+								value="{{ $expense_type->monthly_amount }}"
+							@endif
+						>
 					</div>
 				</div>
 				<fieldset class="form-group">
 					<legend>Set an end date for the recurring expense?</legend>
 					<div class="radio">
 						<label>
-							<input type="radio" name="set_recurring_end_date" data-toggle_target="set_recurring_end_hide" value="1" {{ $expense_type->set_recurring_end_date === "1" ? "checked":"" }}>
+							<input type="radio" name="set_recurring_end_date" data-toggle_target="set_recurring_end_hide" value="1"
+								@if(!is_null(old('set_recurring_end_date')))
+									{{ old('set_recurring_end_date') == '1'? 'checked':'' }}
+								@else
+									{{ $expense_type->set_recurring_end_date == '1' ? 'checked':'' }}
+								@endif
+							>
 							Yes
 						</label>
 					</div>
 					<div class="radio">
 						<label>
-							<input type="radio" name="set_recurring_end_date" data-toggle_target="set_recurring_end_hide" value="0" {{ $expense_type->set_recurring_end_date === "0" ? "checked":"" }}>
+							<input type="radio" name="set_recurring_end_date" data-toggle_target="set_recurring_end_hide" value="0"
+								@if(!is_null(old('set_recurring_end_date')))
+									{{ old('set_recurring_end_date') === '0' ? 'checked':''}}
+								@else
+									{{ $expense_type->set_recurring_end_date === '0' ? 'checked':'' }}
+								@endif
+							>
 							No
 						</label>
 					</div>
 				</fieldset>
-				<div class="set_recurring_end_hide {{ $expense_type->set_recurring_end_date === "1" ? "" : "display-none" }}">
+				<div class="set_recurring_end_hide
+					@if(!is_null(old('set_recurring_end_date')))
+						{{ old('set_recurring_end_date') == '1' ? '':'display-none' }}
+					@else
+						{{ $expense_type->set_recurring_end_date === '1' ? '' : 'display-none' }}
+					@endif
+				">
 					<div class="form-group">
 						<label for="recurring-end-date">Recurring End Date</label>
-						<input type="date" id="recurring-end-date" name="recurring_end_date" class="form-control" value="{{ $expense_type->recurring_end_date }}">
+						<input type="date" id="recurring-end-date" name="recurring_end_date" class="form-control"
+							@if(old('recurring_end_date'))
+								value="{{ old('recurring_end_date') }}"
+							@else
+								value="{{ $expense_type->recurring_end_date }}"
+							@endif
+						>
 						<span class="help-block">An expense will be added each month starting next month up to and including the month of the date provided.</span>
 					</div>
 				</div>
 			</div>
-						<button type="submit" class="btn btn-primary">Submit</button>
+			<button type="submit" class="btn btn-primary">Submit</button>
 			<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#myModal">
 				Delete expense
 			</button>
